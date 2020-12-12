1f6fc672f577722ec5745dc04793736f3cc999f2
MarkusH17 <markushughes1733@gmail.com>
2020-12-11 22:18:09 -0800

    "Added GamingDevil.js and main.css"

---
+++ GamingDevil.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <script src = "GamingDevil.js"></script>
+    </head>
+
+    <body>
+        <div class="gamingDevilClass" id="gamingDevilId">
+            <input type="button">GamingDevil</input>    
+        </div>
+    </body>
+</html>
